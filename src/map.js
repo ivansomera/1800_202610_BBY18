49f4f7857cab1037c0aff29e5bb11fe86cf9fbaa
ivansomera <ivansomera@hotmail.com>
2026-03-19 15:48:08 -0700

    popup displaying. working on fixing bug

--- src/map.js
+++ src/map.js
@@ -39,6 +39,7 @@ function showMap() {
   }
 }
 
+// Display gems from database
 async function showGems(map) {
   const snapshot = await getGems();
 
@@ -56,6 +57,77 @@ async function showGems(map) {
     new maplibregl.Marker({ element: el })
       .setLngLat([doc.lng, doc.lat])
       .addTo(map);
+
+    // add a popup
+    map.on("click", () => {
+      let description = "";
+
+      description += ` 
+          <div class="card-body">
+            <h5 class="card-title">${doc.name}</h5>
+            <ul class="d-flex gap-3 p-0">
+              <li class="card-location">${doc.location}</li>
+              <li class="card-cuisine">${doc.cuisine}</li>
+              <li class="card-cost">${doc.cost}</li>
+            </ul>
+            <ul class="d-flex p-0 gap-3 list-unstyled">
+              <li>
+                <a
+                  href="#"
+                  class="d-flex flex-column card-link text-decoration-none align-items-center"
+                  ><img
+                    src="public/images/menu.svg"
+                    alt="Menu icon"
+                    width="24"
+                    height="24"
+                  />Menu</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="d-flex flex-column card-link text-decoration-none align-items-center"
+                  ><img
+                    src="public/images/reviews.svg"
+                    alt="Reviews icon"
+                    width="24"
+                    height="24"
+                  />Reviews</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="d-flex flex-column card-link text-decoration-none align-items-center"
+                  ><img
+                    src="public/images/map.svg"
+                    alt="Map icon"
+                    width="24"
+                    height="24"
+                  />Location</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="d-flex flex-column card-link text-decoration-none align-items-center"
+                  ><img
+                    src="public/images/favorite.svg"
+                    alt="Heart icon"
+                    width="24"
+                    height="24"
+                  />Favorite</a
+                >
+              </li>
+            </ul>
+          </div>
+        `;
+
+      new maplibregl.Popup()
+        .setLngLat([doc.lng, doc.lat])
+        .setHTML(description)
+        .addTo(map);
+    });
   });
 }
 
