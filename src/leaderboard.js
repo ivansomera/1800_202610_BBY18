2ffcd0c191c33363ff6c448b3f3a5dedc6ea0e46
zylliansaab <zylliansaab@gmail.com>
2026-04-13 15:47:16 -0700

    added functional leaderboard

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -4,6 +4,7 @@ import { collection, query, orderBy, getDocs } from "firebase/firestore";
 let ascending = false;
 
 async function renderLeaderboard() {
+  let i = 1;
   const order = ascending ? "asc" : "desc";
   const q = query(collection(db, "users"), orderBy("points", "desc"));
   const snapshot = await getDocs(q);
@@ -13,10 +14,25 @@ async function renderLeaderboard() {
 
   snapshot.forEach((doc) => {
     const { name, points } = doc.data();
-    const item = document.createElement("li");
-    item.textContent = `${name}: ${points} points`;
-    list.appendChild(item);
+    const row = document.createElement("tr");
+    const rowHeadStart = document.createElement("th");
+    const rowBodyStart = document.createElement("td");
+    const rowPoints = document.createElement("td");
+
+    if (i % 2 != 0) {
+      row.classList.add('table-warning');
+    } else {
+      row.classList.add('table-info');
+    }
+
+    rowHeadStart.textContent = `${i}`;
+    rowBodyStart.textContent = `${name}`;
+    rowPoints.textContent = `${points}`
+    row.append(rowHeadStart, rowBodyStart, rowPoints);
+    list.append(row);
+    i++;
+  
   });
-}
+} 
 
 renderLeaderboard();
